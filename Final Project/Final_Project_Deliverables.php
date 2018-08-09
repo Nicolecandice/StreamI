e66cd8410a1528a6860adb30c519a387ad2b0e60
nicolecandice <nicolecoope@gmail.com>
2018-08-09 11:09:03 -0400

    Final Project

---
+++ Final Project/Final_Project_Deliverables.php
@@ -0,0 +1,7 @@
+Deliverables #1 (July 21, 2018) – Plan, Design, and Research
+
+Deliverables #2 (July 28, 2018) – Ready MVP (Working App!), Mini-Presentation
+
+Deliverables #3 (Aug 4, 2018) – Project Polish, Functionality Push, Final Plan 
+
+Final Presentation (Aug 11, 2018) – Presentation Time!
